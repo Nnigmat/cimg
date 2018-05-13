5a6cfbfb6dd1774f00f2fba54553d0afe55efaeb
NNigmat <nnigmat@gmail.com>
2018-05-13 23:11:27 +0300

    Add possibility to choose how much numbers you want to get and comments

--- cimg.py
+++ cimg.py
@@ -5,49 +5,55 @@ import sys
 import os
 from collections import defaultdict
 
+# Variables declaration
 home = '/home/nigma/'
+path = None
+nOfColors = 20 
 
+# Try to read path to image or its name
 try:
-    arg = str(sys.argv[1]) 
+    path = str(sys.argv[1]) 
 except:
     print("Sorry bro, you haven't add file")
-    sys.exit()
 
-exten = None
+# Try to read how many colors we need to return to the user
 try:
-    exten = arg.split('.')[1]
+    nOfColors = int(sys.argv[2])
 except:
-    pass
+    pass    
 
-if arg != '' and not '/home' in arg:
-   im = Image.open(os.getcwd() + '/' + arg)
-elif arg != '' and '~/' in arg:
-   im = Image.open(home + arg)
+# Open image
+# If user pass to us path with /home... or ~/ we need to find this file into filesystem 
+# Otherwise this image contains in current directory, or not :)
+im = None
+if path and path != '' and (not '/home' in path or not '~/' in path):
+   im = Image.open(os.getcwd() + '/' + path)
+elif path and path != '' and '~/' in path:
+   im = Image.open(home + path)
+elif path and path != '':
+   im = Image.open(path)
 else:
-   im = Image.open(arg)
+   print('Sorry but path to image or its name is invalid')
+   sys.exit()
 
-if not exten or exten.lower() != 'gif':
-    by_color = defaultdict(int)
-    for pixel in im.getdata():
-        print(pixel)
-        by_color[pixel] += 1
-else:
-    rgbim = im.convert('RGB')
-    print(dir(rgbim))
-    print(rgbim.getpixel((0,0)))
-    print(rgbim.size)
-    
-    by_color = defaultdict(int)
-    for pixel in rgbim.getdata():
-        by_color[pixel] += 1
 
+# Convert opened image to RGB format
+# We do this, because for example Gif images stores in another format
+im = im.convert('RGB') 
+
+# Count all pixels in image
+by_color = defaultdict(int)
+for pixel in im.getdata():
+    by_color[pixel] += 1
+
+
+# Sort all elements in dictionary of pixels and return n the most used
 res = ''
 i = 0
-print(sorted(by_color, key=by_color.get, reverse=True))
 for el in sorted(by_color, key=by_color.get, reverse=True):
     res += '#' + str(hex(el[0]))[2:].upper() + str(hex(el[1]))[2:].upper() + str(hex(el[2]))[2:].upper() + ', '
     i += 1
-    if i == 50:
+    if i == nOfColors: 
         break
 
 print(res.rstrip(', ')) 
