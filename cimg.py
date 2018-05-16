5c73d6df06bbc3bcf64e877f150a25f1852d7867
NNigmat <nnigmat@gmail.com>
2018-05-16 20:07:06 +0300

    Change looking of the output, it use true color, so if terminal can't in true color i guess it won't work

--- cimg.py
+++ cimg.py
@@ -5,6 +5,13 @@ import sys
 import os
 from collections import defaultdict
 
+if __name__ != '__main__':
+    sys.exit()
+
+
+
+
+
 # Variables declaration
 home = '/home/nigma/'
 path = None
@@ -40,8 +47,9 @@ else:
 
 # Convert opened image to RGB format
 # We do this, because for example Gif images stores in another format
+im = im.convert('RGBA')
 im = im.filter(ImageFilter.GaussianBlur(10))
-im = im.convert('RGB').convert('P', palette = Image.ADAPTIVE, colors=nOfColors).convert('RGB', palette = Image.ADAPTIVE)
+im = im.convert('P', palette = Image.ADAPTIVE, colors=nOfColors).convert('RGB', palette = Image.ADAPTIVE)
 
 # Count all pixels in image
 by_color = defaultdict(int)
@@ -51,12 +59,17 @@ for pixel in im.getdata():
 
 # Sort all elements in dictionary of pixels and return n the most used
 res = ''
+color_res = ''
 i = 0
-for el in sorted(by_color, key=by_color.get, reverse=True):
+res_array = sorted(by_color, key=by_color.get, reverse=True)
+res_array.sort(key=sum, reverse=True)
+for el in res_array:
     color = '' + str(hex(el[0]))[2:].upper() + str(hex(el[1]))[2:].upper() + str(hex(el[2]))[2:].upper() 
-    res += '\x1b[48;2;' + str(el[0]) + ';' + str(el[1]) + ';' + str(el[2]) + 'm' + color + '\x1b[0m'
+    color_res += '#' + color + ', ' 
+    res += '\x1b[48;2;' + str(el[0]) + ';' + str(el[1]) + ';' + str(el[2]) + 'm                  \x1b[0m  #' + color + '\n'
     i += 1
-    if i == nOfColors: 
+    if i >= nOfColors: 
         break
 
+res += color_res.strip(', ')
 print(res) 
