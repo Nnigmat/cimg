abf20d20f7ded6ec688412943f8a751389cbbf02
NNigmat <nnigmat@gmail.com>
2018-05-16 21:18:40 +0300

    Add windowed output and flag '-c'

--- cimg.py
+++ cimg.py
@@ -5,17 +5,11 @@ import sys
 import os
 from collections import defaultdict
 
-if __name__ != '__main__':
-    sys.exit()
-
-
-
-
-
 # Variables declaration
 home = '/home/nigma/'
 path = None
 nOfColors = 20 
+console = False
 
 # Try to read path to image or its name
 try:
@@ -30,19 +24,22 @@ try:
 except:
     pass    
 
+# Try to read where we need to print result, to console or further to the window application
+console = True if '-c' in sys.argv else False
+
 # Open image
 # If user pass to us path with /home... or ~/ we need to find this file into filesystem 
 # Otherwise this image contains in current directory, or not :)
 im = None
 if path and path != '' and (not '/home' in path or not '~/' in path):
-   im = Image.open(os.getcwd() + '/' + path)
+    im = Image.open(os.getcwd() + '/' + path)
 elif path and path != '' and '~/' in path:
-   im = Image.open(home + path)
+    im = Image.open(home + path)
 elif path and path != '':
-   im = Image.open(path)
+    im = Image.open(path)
 else:
-   print('Sorry but path to image or its name is invalid')
-   sys.exit()
+    print('Sorry but path to image or its name is invalid')
+    sys.exit()
 
 
 # Convert opened image to RGB format
@@ -57,19 +54,23 @@ for pixel in im.getdata():
     by_color[pixel] += 1
 
 
-# Sort all elements in dictionary of pixels and return n the most used
-res = ''
-color_res = ''
-i = 0
 res_array = sorted(by_color, key=by_color.get, reverse=True)
 res_array.sort(key=sum, reverse=True)
-for el in res_array:
-    color = '' + str(hex(el[0]))[2:].upper() + str(hex(el[1]))[2:].upper() + str(hex(el[2]))[2:].upper() 
-    color_res += '#' + color + ', ' 
-    res += '\x1b[48;2;' + str(el[0]) + ';' + str(el[1]) + ';' + str(el[2]) + 'm                  \x1b[0m  #' + color + '\n'
-    i += 1
-    if i >= nOfColors: 
-        break
+if __name__ == '__main__' and console: 
+    # Sort all elements in dictionary of pixels and return n the most used
+    res = ''
+    color_res = ''
+    i = 0
+    for el in res_array:
+        color_res += '#' + color + ', ' 
+        color = '' + str(hex(el[0]))[2:].upper() + str(hex(el[1]))[2:].upper() + str(hex(el[2]))[2:].upper() 
+        res += '\x1b[48;2;' + str(el[0]) + ';' + str(el[1]) + ';' + str(el[2]) + 'm                  \x1b[0m  #' + color + '\n'
+        i += 1
+        if i >= nOfColors: 
+            break
 
-res += color_res.strip(', ')
-print(res) 
+    res += color_res.strip(', ')
+    print(res) 
+else:
+    from main import drawColors 
+    drawColors(res_array, nOfColors)
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QToolTip,  QApplication, QWidget, QPushButton
+from PyQt5 import QtGui
 
-if __name__ == '__main__':
+def drawColors(colors, n):
+   btnWidth = 50
+   btnHeight = 600
+   btnStartWidthPosition = 25
+   btnStartHeightPosition = 25
+   windowWidth = btnStartWidthPosition*2 + btnWidth * n
+   windowHeight = btnHeight + btnStartHeightPosition*2
+
    app = QApplication(sys.argv)
    w = QWidget()
-   w.resize(600, 600)
+   w.resize(windowWidth, windowHeight)
    w.move(300, 300)
    w.setWindowTitle('Cimg')
-   w.show()
 
+   # QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+   # btn = QPushButton('   ', w)
+   # btn.setToolTip('This is a dsakjflslkjf')
+   # btn.resize(btnWidth, btnHeight)
+   # btn.setStyleSheet('background-color: #DDFFDD')
+      
+   for i in range(n):
+       el = colors[i]
+       color = '' + str(hex(el[0]))[2:].upper() + str(hex(el[1]))[2:].upper() + str(hex(el[2]))[2:].upper() 
+       btn = QPushButton('   ', w)
+       btn.resize(btnWidth, btnHeight)
+       btn.move(btnWidth*i + btnStartWidthPosition, btnStartHeightPosition)
+       btn.setStyleSheet('background-color: #' + color) 
+   w.show()
    sys.exit(app.exec_())
